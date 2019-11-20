26265dbeb2ad19151899b9754be7f8935c1bb634
Andy He <andyhe@berkeley.edu>
2019-11-19 23:12:26 -0800

    Hexworld in progress

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,16 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    /**
+     * add Hexagon function
+     * @param world the world to draw on
+     * @param x the starting point at the left bottom coordinate
+     * @param y the starting point at the left bottom coordinate
+     * @param base the base size to begin with
+     * @param t the tile to draw
+     */
+    public static void addHexagon(TETile[][]world,int x,int y,int base, TETile t) {
+        world
+    }
+    public static void
 }
