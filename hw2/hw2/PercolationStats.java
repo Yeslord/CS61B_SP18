1ea228f5ac89c6408234716ffc2175c1ec19fc18
Andy He <andyhe@berkeley.edu>
2019-12-02 23:21:57 -0800

    hw2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,52 +1,49 @@
+
 package hw2;
 
-import edu.princeton.cs.introcs.StdRandom;
-import edu.princeton.cs.introcs.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-    private double[] C;
+    // private int N;
+    private double[] data;
 
-    // perform T independent experiments on an N-by-N grid
+
     public PercolationStats(int N, int T, PercolationFactory pf) {
-
-        if (N <= 0 || T <= 0) throw new IllegalArgumentException("Illegal Arguments!");
-
-
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        data = new double[T];
         for (int i = 0; i < T; i += 1) {
             Percolation p = pf.make(N);
-            while (!p.percolates()) {
-                int rdmRow = StdRandom.uniform(N);
-                int rdmCol = StdRandom.uniform(N);
-                p.open(rdmRow, rdmCol);
-                }
-            C[i]=(double)p.numberOfOpenSites();
+            while (p.percolates() == false) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                p.open(row, col);
             }
+            data[i] = (double) p.numberOfOpenSites() / (N * N);
         }
+    }
 
-
-    // sample mean of percolation threshold
     public double mean() {
-        return StdStats.mean(C);
-
+        return StdStats.mean(data);
     }
 
-
-    // sample standard deviation of percolation threshold
     public double stddev() {
-        return StdStats.stddev(C);
+        return StdStats.stddev(data);
     }
 
-
-    // low endpoint of 95% confidence interval
     public double confidenceLow() {
-        return mean() - 1.96 * Math.sqrt(stddev() / C.length);
+        double mean = mean();
+        double stddev = stddev();
+        return mean - (1.96 * stddev) / Math.sqrt(data.length);
     }
 
-
-    // high endpoint of 95% confidence interval
     public double confidenceHigh() {
-        return mean() + 1.96 * Math.sqrt(stddev() / C.length);
+        double mean = mean();
+        double stddev = stddev();
+        return mean + (1.96 * stddev) / Math.sqrt(data.length);
     }
 
 }
